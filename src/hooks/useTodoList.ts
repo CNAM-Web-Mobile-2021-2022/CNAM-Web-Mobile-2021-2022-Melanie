0b7835914b93bcdf6333a82550103c7ae7f2de84
Melaniem18 <73828280+Melaniem18@users.noreply.github.com>
2022-05-27 13:30:39 +0200

    add app

---
+++ src/hooks/useTodoList.ts
@@ -0,0 +1,9 @@
+import { useState, useEffect } from 'react';
+import { isPlatform } from '@ionic/react';
+
+export function useTodoList() {
+    const addTask = async () => {
+        
+    };
+
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,59 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import {
+  IonHeader, IonToolbar, IonSegment, IonSegmentButton, IonLabel, IonInput,
+  IonItem, IonButton, IonCard, IonCardContent, IonCheckbox, IonFooter, IonList, IonBadge, IonPage
+} from '@ionic/react';
 import './Home.css';
+import React, { useState } from 'react';
+// import { useTodoList } from '../hooks/useTodoList';
+
 
 const Home: React.FC = () => {
+  // const { addTask } = useTodoList();
+  const [task, setTask] = useState<string>();
+  const [checked, setChecked] = useState(false);
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonSegment onIonChange={e => console.log('Segment selected', e.detail.value)}>
+            <IonSegmentButton value="faire">
+              <IonItem>
+                <IonBadge slot='end' color='secondary'>1</IonBadge>
+                <IonLabel>A faire</IonLabel>
+              </IonItem>
+            </IonSegmentButton>
+            <IonSegmentButton value="tous">
+              <IonLabel>Tous</IonLabel>
+            </IonSegmentButton>
+          </IonSegment>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
+
+      <IonCard>
+        <IonCardContent>
+          <IonList>
+            <IonItem>
+              <small>Votre liste est vide, bravo !</small>
+            </IonItem>
+            <IonItem>
+              <IonLabel>Tâche à faire</IonLabel>
+              <IonCheckbox slot='start' checked={checked} onIonChange={e => setChecked(e.detail.checked)} />
+            </IonItem>
+          </IonList>
+        </IonCardContent>
+      </IonCard>
+
+      <IonFooter>
+        <IonCard class='inputTask'>
+          <IonItem>
+            <IonInput value={task} placeholder="Qu'avez vous en tête ?" onIonChange={e => setTask(e.detail.value!)}>
+            </IonInput>
+            <IonButton class='btnAdd' slot='end' color='secondary' shape='round' >
+              Créer
+            </IonButton>
+          </IonItem>
+        </IonCard>
+      </IonFooter>
     </IonPage>
   );
 };
